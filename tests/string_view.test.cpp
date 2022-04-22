fbfff9bde6659c671558b5ec298facae71081529
bmathieugit <bmathieugit@gmail.com>
2022-04-22 17:25:40 +0200

    update range commit head

--- tests/string_view.test.cpp
+++ tests/string_view.test.cpp
@@ -14,15 +14,16 @@ int main()
   testok += *(hello.end() - 1) == 'o' ? 1 : 0;
   testok += hello[0] == 'h' ? 1 : 0;
   testok += hello[4] == 'o' ? 1 : 0;
-  testok += hello.after('h').size() == 4 ? 1 : 0;
 
   testok += hello.after('h').size() == 4 ? 1 : 0;
+  testok += hello.after('h').size() == 4 ? 1 : 0;
   testok += hello.before('h').size() == 0 ? 1 : 0;
-  testok += hello == "hello" ? 1 : 0;
-  testok += hello.before('h') == "" ? 1 : 0;
-  testok += hello.after('h') == "ello" ? 1 : 0;
-  testok += hello.starts_with("hel") ? 1 : 0;
-  testok += !hello.starts_with("el") ? 1 : 0;
+  testok += hello == lib::svfrom("hello") ? 1 : 0;
+  testok += hello.before('h') == lib::svfrom("") ? 1 : 0;
+
+  testok += hello.after('h') == lib::svfrom("ello") ? 1 : 0;
+  testok += hello.starts_with(lib::svfrom("hel")) ? 1 : 0;
+  testok += !hello.starts_with(lib::svfrom("el")) ? 1 : 0;
 
   std::printf("test OK %d/%d\n", testok, totaltest);
 }
