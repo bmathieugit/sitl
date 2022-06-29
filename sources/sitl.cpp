487a095c0529a57c3727e9cfebbd59c372ff0d42
bmathieugit <bmathieugit@gmail.com>
2022-06-29 16:16:47 +0200

    clean sitl main

--- sources/sitl.cpp
+++ sources/sitl.cpp
@@ -7,34 +7,33 @@
 
 int main(int argc, char **argv)
 {
+  namespace logger = sitl::logger;
   sitl::CommandLine args(argc, argv);
 
   if (args.contains(sitl::sr("file")))
   {
     sitl::StringCRange fname = args.value(sitl::sr("file"));
-    sitl::logger::debug("file name found, fname");
+    logger::debug("file name found, fname");
   }
 
-  sitl::StringCRange src = sitl::sr(
-      "struct string\n"
-      "begin\n"
-      "  param char b\n"
-      "  param char e\n"
-      "end\n"
-      "let int i '(+ i (- 1 12))'");
-  sitl::Vector<sitl::Token> silt_tokens = sitl::ProceduralGTokenizer()(src);
+  logger::debug(
+      "result ",
+      sitl::SitlAnalyser()(
+          sitl::ProceduralGTokenizer()(
+              sitl::sr(
+                  "struct string\n"
+                  "begin\n"
+                  "  param char b\n"
+                  "  param char e\n"
+                  "end\n"
+                  "let int i '(+ i (- 1 12))'"))));
 
-  for (auto &&token : silt_tokens)
-    sitl::logger::debug("token type : ", (int)token.type, ", value : ", token.value);
-
-  sitl::logger::debug("result ", sitl::SitlAnalyser()(silt_tokens));
-  sitl::StringCRange src2 = sitl::sr("(add 1 (minus 2 \"toto\"))");
-  sitl::Vector<sitl::Token> lisp_tokens = sitl::ExpressionGTokenizer()(src2);
-
-  for (auto &&token : lisp_tokens)
-    sitl::logger::debug("token type : ", (int)token.type, ", value : ", token.value);
-
-  sitl::logger::debug("result : ", sitl::ExpressionAnalyser()(lisp_tokens)); 
+  logger::debug(
+      "result : ",
+      sitl::ExpressionAnalyser()(
+          sitl::ExpressionGTokenizer()(
+              sitl::sr(
+                  "(add 1 (minus 2 \"toto\"))"))));
 
   return 0;
 }
